ec49c0c910107aad43aad9f4a936c357e7d4e574
Emmanuel-bot-rgb <pe699674@gmail.com>
2025-02-04 04:20:45 +0500

    updated

--- src/app/page.js
+++ src/app/page.js
@@ -15,13 +15,17 @@ export default function Home() {
         </div>
 
       
-        <h1 data-aos="fade-right" className="font-bold md:text-6xl sm:text-6xl text-indigo-200 mb-4">
-          Hi there <br />
-          I am{" "}
-          <a className="underline  text-indigo-400 decoration-indigo-500 underline-offset-3">
-            Mahnoor Khushbakht
-          </a>
-        </h1>
+        <h1
+  data-aos="fade-right"
+  className="font-bold text-4xl sm:text-5xl md:text-6xl text-indigo-200 mb-4"
+>
+  Hi there <br />
+  I am{" "}
+  <a className="underline text-indigo-400 decoration-indigo-500 underline-offset-3">
+    Mahnoor Khushbakht
+  </a>
+</h1>
+
 
         
         <div data-aos="fade-right" className=" md:text-4xl sm:text-3xl text-3xl text-indigo-300">
