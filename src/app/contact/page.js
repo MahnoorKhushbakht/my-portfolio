01a54fc4f6d0bb93aecf49e829822403285168d8
Emmanuel-bot-rgb <pe699674@gmail.com>
2025-02-04 22:56:59 +0500

    updated

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -72,7 +72,7 @@ export default function Contact() {
           style={{ filter: "grayscale(1) contrast(1.2) opacity(0.4)" }}
         ></iframe>
       </div>
-      <div className="container px-5 py-24 mx-auto flex">
+      <div className="container px-2 py-24 mx-auto flex">
         <div className="lg:w-1/3 md:w-1/2 bg-indigo-400 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10 shadow-md">
           <h2 className="text-indigo-100 text-lg mb-1 font-medium title-font">Get in Touch</h2>
           <p className="leading-relaxed mb-5 text-indigo-100">
@@ -109,7 +109,7 @@ export default function Contact() {
             <button
               type="submit"
               aria-label="Send feedback"
-              className="text-indigo-400 bg-indigo-100 w-full border-0 py-2 px-6 focus:outline-none hover:bg-indigo-400 hover:text-indigo-100 rounded text-lg"
+              className="text-indigo-400 bg-indigo-100 w-full border-0 py-2 px-6 focus:outline-none hover:bg-indigo-500 hover:text-indigo-100 rounded text-lg"
               disabled={loading}
             >
               {loading ? (
